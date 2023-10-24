8cbfd482287f3772f31528766ecc88de75ca59bd
kivetat-tatevik <kivetat.85@yandex.ru>
2023-10-24 12:18:35 +0200

    Initial Commit

---
+++ CheckMeSwiftUI/CheckMeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckMeSwiftUIApp.swift
+//  CheckMeSwiftUI
+//
+//  Created by Tatevik Khunoyan on 24.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CheckMeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
